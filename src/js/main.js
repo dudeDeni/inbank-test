c82bbc6eac5fe3abeb62b1a5bfa19be4d15c54e5
Daniil Latt <daniillatt@gmail.com>
2024-01-17 23:40:15 +0200

    still in progress

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '../scss/style.scss'
+import "bootstrap-icons/font/bootstrap-icons.css"
 import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
