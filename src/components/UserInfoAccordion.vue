dfe194378196c1167efd325a040e811d8ccd8354
Daniil Latt <daniillatt@gmail.com>
2024-01-18 07:50:10 +0200

    info transition fix

--- src/components/UserInfoAccordion.vue
+++ src/components/UserInfoAccordion.vue
@@ -2,8 +2,8 @@
   <div class="mb-4" id="accordionExample">
     <div class="">
       <div v-if="formEdit" class="py-3 d-flex justify-content-between align-items-center w-100 bg-info rounded-pill border-4 border-info">
-        <div class="col-3 offset-1 fs-6 d-flex justify-content-center" style="font-size: 18px;">
-          <label>
+        <div class="col-4 fs-6 d-flex justify-content-center" >
+          <label class="fs-6">
             Change your contact Info
           </label>
         </div>
@@ -18,22 +18,24 @@
           <button class="bg-primary rounded-circle border-0" style="width: 31px;height: 31px;" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" @click="editForm()"><i class="bi-chevron-right text-textWhite"></i></button>
         </div>
       </div>
-      <div v-else="!formEdit" class="py-3 d-flex justify-content-between align-items-center w-100 bg-info rounded-top-5 border border-bottom-0 border-4 border-light">
-        <div class="col-6 offset-3" style="font-size: 18px;">Anna Maria Tamm Rodriguez Espinosa</div>
-        <div class="col-1 ">
-          <button class="bg-textWhite rounded-circle border-0 " style="width: 31px;height: 31px;" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" @click="editForm()"><i class="bi bi-pen-fill"></i></button>
+      <div v-else="!formEdit" class="border border-4 border-light" style="border-radius: 35px">
+        <div class="py-3 d-flex justify-content-between align-items-center bg-info" style="border-radius: 30px 30px 0px 0px;">
+          <div class="col-6 offset-3" style="font-size: 18px;">Anna Maria Tamm Rodriguez Espinosa</div>
+          <div class="col-1 ">
+            <button class="bg-textWhite rounded-circle border-0 " style="width: 31px;height: 31px;" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" @click="editForm()"><i class="bi bi-pen-fill"></i></button>
+          </div>
         </div>
-      </div>
-      <div id="collapseOne" class="rounded-bottom-5 border border-top-0 border-4 border-light accordion-collapse collapse show" data-bs-parent="#accordionExample">
-        <div class="row m-0 py-2 text-center ">
-          <div class="col">
-            {{userData.id_code}}
-          </div>
-          <div class="col">
-            +{{userData.phone}}
-          </div>
-          <div class="col">
-            {{userData.email}}
+        <div id="collapseOne" class="py-2 accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div class="row m-0 text-center ">
+            <div class="col">
+              {{userData.id_code}}
+            </div>
+            <div class="col">
+              +{{userData.phone}}
+            </div>
+            <div class="col">
+              {{userData.email}}
+            </div>
           </div>
         </div>
       </div>
